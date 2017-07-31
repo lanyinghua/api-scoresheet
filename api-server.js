234753bcd36a89f57dc201676880afb6e5226c8f
lanyinghua <448786703@qq.com>
2017-07-31 09:53:03 +0800

    7th

---
+++ api-server.js
@@ -0,0 +1,159 @@
+/**
+ * Created by huadu on 7/27/17.
+ */
+
+var express = require('express');
+var bodyParser=require('body-parser');
+var expressapp = express();
+// expressapp.use(bodyParser.json());
+expressapp.use(bodyParser.urlencoded({extended:false}));
+
+var count=0;
+var redis= require('redis');
+var redisclient=redis.createClient();
+
+
+expressapp.get('/students',function(req,res){
+    $('#table').html('');
+    $('#avgmedian').html('');
+    let key=req.body.stuid;//666,23...
+    let keyarr=key.split(',');//keyarr=['666','23'...]
+    let newtr;
+    let totalarr=[];
+    for (let i=0;i<keyarr.length;i++){
+        let infostr=redisclient.get(keyarr[i],function (err,reply) {
+            if (err){
+                res.send('请按正确的格式输入要打印的学生的学号~*^_^*\n400')
+            }else {
+                let infoarr=JSON.parse(infostr);
+                totalarr.push(infoarr.total);
+                newtr="<tr >"+
+                    "<td>"+infoarr.name+"</td>"+
+                    "<td>"+infoarr.id+"</td>"+
+                    "<td>"+infoarr.mat+"</td>"+
+                    "<td>"+infoarr.chi+"</td>"+
+                    "<td>"+infoarr.eng+"</td>"+
+                    "<td>"+infoarr.pro+"</td>"+
+                    "<td>"+infoarr.total+"</td>"+
+                    "</tr>";
+                $('#table').append(newtr);
+                res.send(reply);
+            }
+        })
+    };
+    let totaltr="<tr>"+
+        "<td>"+medianOfTotalOfClass(totalarr)+"</td>"+
+        "<td>"+avgOfTotalOfClass(totalarr)+"</td>"+
+        "</tr>";
+    redisclient.set('medAndAvg',totaltr);
+    redisclient.get('medAndAvg',function (err,reply) {
+       res.send(reply)
+    });
+    $('#avgmedian').append(totaltr);
+});
+
+function medianOfTotalOfClass(arr) {//totalarr:[num]
+    arr=arr.sort(function (compare1,compare2) {
+        return compare1 - compare2
+    });
+    let midIndex=parseInt(arr.length/2);
+    let median=0;
+    if (arr.length%2===0){
+        median=(arr[midIndex-1]+arr[midIndex])/2;
+    }else {
+        median=arr[midIndex];
+    }
+    return median;
+}
+function avgOfTotalOfClass(arr){//arr=totalarr:[num]
+    let totaloftotal=0;
+    for (let i=0;i<arr.length;i++){
+        totaloftotal+=arr[i];
+    }
+    let l=0;l=arr.length;
+    let avgoftotal=0;
+    avgoftotal=(totaloftotal/l).toFixed(2);
+    return avgoftotal;
+}
+
+expressapp.use(express.static('public'));
+expressapp.get('/student', function (req, res) {
+    res.sendFile( __dirname + "/public/web-scoresheet.html" );
+});
+
+
+expressapp.post('/student',function (req,res) {
+    let infoarr={
+       name: req.body.name,
+       id: req.body.id,
+       nation: req.body.nation,
+       klass: req.body.klass,
+       mat: req.body.mat,
+       chi: req.body.chi,
+       eng: req.body.eng,
+       pro: req.body.pro,
+        total:0,
+        avg:0,
+    };
+    if (infoarr.name.length<4 && infoarr.name!==""&&infoarr.id !==''){//如果输入格式正确就会返回
+        infoarr.total=parseInt(infoarr.mat)+parseInt(infoarr.chi)+parseInt(infoarr.eng)+parseInt(infoarr.pro);
+        infoarr.avg=(infoarr.total/4).toFixed(2);
+        let infostr=JSON.stringify(infoarr);
+        redisclient.set(`${infoarr.id}`,infostr);
+        redisclient.get(`${infoarr.id}`,function (err,reply) {
+            res.send(reply);
+        })
+    }else {//如果输入格式不正确
+        res.send('请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）\n400')
+    }
+
+});
+
+expressapp.put('/students/:id',function (req,res) {
+    let infoarr={
+        name: req.body.crtname,
+        id: req.body.crtid,
+        nation: req.body.crtnation,
+        klass: req.body.crtklass,
+        mat: req.body.crtmat,
+        chi: req.body.crtchi,
+        eng: req.body.crteng,
+        pro: req.body.crtpro,
+        total:0,
+        avg:0,
+    };
+    let sameIdstr=redisclient.get(infoarr.id,function (err,reply) {
+        reply
+    });
+    if (!sameIdstr){
+        res.send('请按正确的格式输入');
+    }else {
+        let sameIdArr=JSON.parse(sameIdstr);//same id's student info(str)
+        if (sameIdArr.id===infoarr.id){
+            let infostr=JSON.stringify(infoarr);
+            redisclient.set(infoarr.id,infostr);
+            redisclient.get(infoarr.id,function (err,reply) {
+                res.send(reply);
+            });
+        }else{
+            res.send('请按正确的格式输入');
+        }
+    }
+
+});
+
+expressapp.delete('/students/:id',function (req,res) {
+    let key=req.body.deleteid;
+    redisclient.get('key',function (err,reply) {
+        if (err){
+            res.send(`该学生不存在\n错误码404`)
+        }else {
+            redisclient.del('key');
+            res.send('该学生已成功删除')
+        }
+    })
+});
+
+expressapp.listen(8000,'localhost',function(){
+    console.log('express is listening');
+});
